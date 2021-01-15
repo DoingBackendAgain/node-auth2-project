2081b9740c544f8a10e49820652b72e331b3e23b
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-14 20:34:28 -0500

    middleware created

---
+++ middleware.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken")
+
+function restrict(){
+    return async (req, res, next) => {
+        const authError = {
+            message: "Invalid Credentials"
+        }
+
+        try {
+            const token = req.headers.authorization
+
+            if(!token){
+                return res.status(401).json(authError)
+            }
+
+            jwt.verify(token, "keep it secret", (err, decoded) => {
+                if(err) {
+                    return res.status(401).json(authError)
+                }
+
+                req.token = decoded
+
+                next()
+            })
+        }
+        catch(err){
+            next(err)
+        }
+    }
+}
+
+module.exports = {
+    restrict
+}
