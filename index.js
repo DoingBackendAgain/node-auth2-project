92279ef189720bc0a007f4c47d380c9d97ea1186
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-15 21:20:56 -0500

    index

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //require("dotenv").config() this goes here or in json
+
+const server = require("./server")
+
+const PORT = process.env.PORT || 1000;
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
